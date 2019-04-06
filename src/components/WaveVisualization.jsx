87ea752e7db699bd816295a976bb07fd728ac699
s1lentem <tema.yarmosh@gmail.com>
2019-04-06 16:24:39 +0300

    Added visualization component

---
+++ src/components/WaveVisualization.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class WaveVisualization extends React.Component {
+  render(){
+    return (
+      <div>
+        <canvas>
+        </canvas>
+      </div>
+    );
+  }
+}
+
+export default WaveVisualization;
